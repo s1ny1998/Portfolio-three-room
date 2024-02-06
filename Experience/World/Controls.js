233e621d80aba3861075f30cdc58cde8b72b98b1
sanmitra nagaraj <sanmitra.nagaraj@gmail.com>
2024-02-06 23:35:52 +0530

    added scroll trigger

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,15 +1,38 @@
 import Experience from "../Experience"
 import * as THREE from "three";
 import GSAP from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default class Controls {
     constructor() {
         this.experience = new Experience();
         this.scene = this.experience.scene;
+        this.sizes = this.experience.sizes;
         this.resources = this.experience.resources;
         this.time = this.experience.time;
         this.camera = this.experience.camera;
+        this.room = this.experience.world.room.actualRoom;
+        GSAP.registerPlugin(ScrollTrigger);
+        this.setPath();
 
+    }
+
+    setPath(){
+        console.log(this.room);
+        this.timeline = new GSAP.timeline();
+        this.timeline.to(this.room.position, {
+            x: () =>{ 
+                return this.sizes.width * 0.0012
+            },
+            scrollTrigger: {
+                trigger: ".first-move",
+                markers: true,
+                start: "top top",
+                end: "bottom bottom",
+                scrub: 0.6,
+                invalidateOnRefresh: true,
+            },
+        });
     }
     
 
